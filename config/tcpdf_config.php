f20445183f08885e3873ef91813ffb246ae75a9e
weerapoj <w.tayod@gmail.com>
2024-09-10 14:43:26 +0700

    Add Font Unicode

--- config/tcpdf_config.php
+++ config/tcpdf_config.php
@@ -154,7 +154,7 @@ define ('PDF_MARGIN_RIGHT', 15);
 /**
  * Default main font name.
  */
-define ('PDF_FONT_NAME_MAIN', 'helvetica');
+define ('PDF_FONT_NAME_MAIN', 'arial');
 
 /**
  * Default main font size.
@@ -164,7 +164,7 @@ define ('PDF_FONT_SIZE_MAIN', 10);
 /**
  * Default data font name.
  */
-define ('PDF_FONT_NAME_DATA', 'helvetica');
+define ('PDF_FONT_NAME_DATA', 'arial');
 
 /**
  * Default data font size.
